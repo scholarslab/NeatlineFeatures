946c2e57f590662a3db71776e27201a9e60911e4
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2009-10-19 14:59:10 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@1696 d187d172-3112-0410-8ee7-ad866068e984

--- plugin.php
+++ plugin.php
@@ -12,15 +12,15 @@ define('NEATLINEFEATURES_PLUGIN_VERSION', get_plugin_ini('neatlinefeatures', 've
 define('NEATLINEFEATURES_PLUGIN_DIR', dirname(__FILE__));
 
 /*
-define('NEATLINE_GEOSERVER', 'http://localhost:8080/geoserver');
-define('NEATLINE_GEOSERVER_NAMESPACE_PREFIX', 'neatline');
-define('NEATLINE_GEOSERVER_NAMESPACE_URL', 'http://www.neatline.org');
-define('NEATLINE_GEOSERVER_ADMINUSER', 'admin');
-define('NEATLINE_GEOSERVER_ADMINPW', 'geoserver');
+ define('NEATLINE_GEOSERVER', 'http://localhost:8080/geoserver');
+ define('NEATLINE_GEOSERVER_NAMESPACE_PREFIX', 'neatline');
+ define('NEATLINE_GEOSERVER_NAMESPACE_URL', 'http://www.neatline.org');
+ define('NEATLINE_GEOSERVER_ADMINUSER', 'admin');
+ define('NEATLINE_GEOSERVER_ADMINPW', 'geoserver');
 
 
-define('NEATLINE_SPATIAL_REFERENCE_SERVICE','http://spatialreference.org/ref');
-*/
+ define('NEATLINE_SPATIAL_REFERENCE_SERVICE','http://spatialreference.org/ref');
+ */
 
 add_plugin_hook('install', 'neatlinefeatures_install');
 add_plugin_hook('uninstall', 'neatlinefeatures_uninstall');
@@ -32,32 +32,29 @@ function neatlinefeatures_uninstall()
 {
 	delete_option('neatlinefeatures_plugin_version');
 }
-/*
-function neatline_install()
+
+function neatlinefeatures_install()
 {
 	$writer = new Zend_Log_Writer_Stream(LOGS_DIR . DIRECTORY_SEPARATOR . "neatline.log");
 	$logger = new Zend_Log($writer);
 
-	set_option('neatline_version', NEATLINE_PLUGIN_VERSION);
+	set_option('neatlinefeatures_version', NEATLINEFEATURES_PLUGIN_VERSION);
 
-	$geoserver_config_addy = NEATLINE_GEOSERVER . "/rest/namespaces" ;
-	$client = new Zend_Http_Client($geoserver_config_addy);
-	$client->setAuth(NEATLINE_GEOSERVER_ADMINUSER, NEATLINE_GEOSERVER_ADMINPW);
+	# now we add 'Historic Map' item type
+	$featitemtype = array(
+     'name'       => "Geographic feature", 
+      'description' => "Geographic feature "
+      );
 
-	if ( !preg_match( NEATLINE_GEOSERVER_NAMESPACE_URL, $client->request(Zend_Http_Client::GET)->getBody() ) ) {
-		$namespace_json =
-	"{'namespace' : { 'prefix': '" . NEATLINE_GEOSERVER_NAMESPACE_PREFIX . "', 'uri': '" . NEATLINE_GEOSERVER_NAMESPACE_URL . "'} }";
-		$response = $client->setRawData($namespace_json, 'text/json')->request(Zend_Http_Client::POST);
-		if ($response->isSuccessful()) {
-		 $logger->log("Neatline GeoServer namespace " . NEATLINE_GEOSERVER_NAMESPACE_PREFIX
-		 . "(" . NEATLINE_GEOSERVER_NAMESPACE_URL . ")" . " added to GeoServer config.", Zend_Log::INFO);
-		}
-		else {
-		 $logger->log("Failed to add Neatline/GeoServer namespace: check  Neatline config.", Zend_Log::ERROR);
-		}
-	}
+      $featitemtypemetadata = array(
+      array(
+              'name'        => "Shape", 
+              'description' => "Shape in WKT form"             
+              ));
+              insert_item_type($featitemtype,$featitemtypemetadata);
+
 }
-*/
+
 
 /**
  * Add the routes from routes.ini in this plugin folder.
