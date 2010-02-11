7f679f340a8eeee62c92e03258d49af34f8cb470
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-11 21:08:39 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@1993 d187d172-3112-0410-8ee7-ad866068e984

--- views/public/features/show.php
+++ views/public/features/show.php
@@ -18,7 +18,7 @@
 		<?php echo js("features/show/init"); ?>
 	</head>
 	<body onload="init()">
-
+		<?php echo $backgroundMap?>
 		 <div id="map" style="height: 400px; width: 700px; 
 border: 1px solid #ccc; float:right;"></div>
 		
