4a333cd70142c0953caae5009915bc374863e79e
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-11 14:43:12 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@1901 d187d172-3112-0410-8ee7-ad866068e984

---
+++ plugin.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @version $Id$
+ * @copyright
+ * @package neatline
+ **/
+
+define('NEATLINEFEATURES_PLUGIN_VERSION', get_plugin_ini('NeatlineFeatures', 'version'));
+define('NEATLINEFEATURES_PLUGIN_DIR', dirname(__FILE__));
+
+
+add_plugin_hook('install', 'neatlinefeatures_install');
+add_plugin_hook('uninstall', 'neatlinefeatures_uninstall');
+add_plugin_hook('define_routes', 'neatlinefeatures_routes');
+//add_filter(array('Form','Item','Dublin Core','Coverage'),"neatlinefeatures_map_widget");
+
+function neatlinefeatures_uninstall()
+{
+	delete_option('neatlinefeatures_plugin_version');
+}
+
+function neatlinefeatures_install()
+{
+	$writer = new Zend_Log_Writer_Stream(LOGS_DIR . DIRECTORY_SEPARATOR . "neatline.log");
+	$logger = new Zend_Log($writer);
+	set_option('neatlinefeatures_version', NEATLINEFEATURES_PLUGIN_VERSION);
+
+}
+
+/**
+ * Add the routes from routes.ini in this plugin folder.
+ *
+ * @return void
+ **/
+function neatlinefeatures_routes($router)
+{
+	$router->addConfig(new Zend_Config_Ini(NEATLINEFEATURES_PLUGIN_DIR .
+	DIRECTORY_SEPARATOR . 'routes.ini', 'routes'));
+}
+
+function neatlinefeatures_map_widget($html,$inputNameStem,$value,$options,$record,$element)
+{
+	return "<div>WINNNN!!!!</div>";
+}
+
