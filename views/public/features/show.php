9bb3a6456b6d7a31742ce525a9fd8d4b1656a3a1
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-11 15:32:38 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@1926 d187d172-3112-0410-8ee7-ad866068e984

--- views/public/features/show.php
+++ views/public/features/show.php
@@ -5,7 +5,13 @@
 ?>
 <html>
 
-	<head/>
+	<head>
+		<title>Neatline feature display</title>
+		<?php 
+			js('OpenLayers');
+		
+		?>
+	</head>
 	<body>
 		<?php
 		 echo item("Dublin Core", "Coverage", $item); ?>
