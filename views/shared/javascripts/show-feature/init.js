6097933239114bb2218825cb698b497aa736b3c9
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-11 17:29:45 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@1949 d187d172-3112-0410-8ee7-ad866068e984

--- views/shared/javascripts/show-feature/init.js
+++ views/shared/javascripts/show-feature/init.js
@@ -1,11 +1,16 @@
 var init = function() {
 	
 	map = new OpenLayers.Map( {
-		projection : new OpenLayers.Projection("EPSG:4326")
+		allOverlays: true,
+		maxExtent: new OpenLayers.Bounds(
+                1549471.9221, 6403610.94, 1550001.32545, 6404015.8
+            )
 	});
 	
 	layer = new OpenLayers.Layer.Vector("feature");
 	layer.addFeatures([ feature ]);
 	map.addLayer(layer);
+	map.addControl(new OpenLayers.Control.NavToolbar());
+
 	map.zoomToExtent(feature.geometry.getBounds());
 }
