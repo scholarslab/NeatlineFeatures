247684ef3550dc8067917d0d130cf96ba7b0f382
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-11 21:40:41 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@2000 d187d172-3112-0410-8ee7-ad866068e984

--- views/shared/javascripts/features/show/init.js
+++ views/shared/javascripts/features/show/init.js
@@ -1,18 +1,28 @@
 var init = function() {
-	
+
 	wgs84 = new OpenLayers.Projection("EPSG:4326");
-	
-	map = new OpenLayers.Map('map',{
+
+	map = new OpenLayers.Map('map', {
 		projection : wgs84,
 		numZoomLevels : 128
 	});
-	
+
 	layer = new OpenLayers.Layer.Vector("feature");
-	layer.addFeatures( feature );
+	layer.addFeatures(feature);
 	map.addLayer(layer);
 	if (backgroundMap.length > 0) {
-		var backgroundlayer =
-			new OpenLayers.Layer.WMS(backgroundMap,"http://scholarslab.org:8080/geoserver/wms", { srs:"EPSG:4326", layers:"neatline:"  + backgroundMap })	
+		var backgroundWMS;
+		Ajax.Request("/maps/wms/" + backgroundMap, {
+			asynchronous : false,
+			onSuccess : function(response) {
+				backgroundWMS = response.text;
+			}
+		});
+		var backgroundlayer = new OpenLayers.Layer.WMS(backgroundMap,
+				backgroundWMS, {
+					srs : "EPSG:4326",
+					layers : "neatline:" + backgroundMap
+				})
 		map.addLayer(backgroundlayer);
 	}
 	map.addControl(new OpenLayers.Control.NavToolbar());
