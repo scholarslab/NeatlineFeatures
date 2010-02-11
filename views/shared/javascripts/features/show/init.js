2dbfadd6ff27f9265c69fb847f274d4004a2da34
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-11 21:10:48 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@1995 d187d172-3112-0410-8ee7-ad866068e984

--- views/shared/javascripts/features/show/init.js
+++ views/shared/javascripts/features/show/init.js
@@ -10,8 +10,9 @@ var init = function() {
 	layer = new OpenLayers.Layer.Vector("feature");
 	layer.addFeatures( feature );
 	map.addLayer(layer);
-	if (backgroundMap) {
-		var backgroundlayer = new OpenLayers.Layer.WMS(background,"http://scholarslab.org:8080/geoserver", { srs:"EPSG:4326", layers:"neatline:"  + background })	
+	if (backgroundMap.length > 0) {
+		var backgroundlayer =
+			new OpenLayers.Layer.WMS(background,"http://scholarslab.org:8080/geoserver", { srs:"EPSG:4326", layers:"neatline:"  + backgroundMap })	
 		map.addLayer(backgroundlayer);
 	}
 	map.addControl(new OpenLayers.Control.NavToolbar());
