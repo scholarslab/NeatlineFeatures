1ff20f9bd5d76236c3490f825d121f9df285684e
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-11 15:24:32 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@1922 d187d172-3112-0410-8ee7-ad866068e984

--- controllers/FeaturesController.php
+++ controllers/FeaturesController.php
@@ -19,8 +19,8 @@ class NeatlineFeatures_FeaturesController extends Omeka_Controller_Action
 		$id = (!$id) ? $this->getRequest()->getParam('id') : $id;
 		$item = $this->findById($id,"Item");
 		//$gml = $this->itemMetadata($item,'Dublin Core', 'Coverage');
-		$r = new ReflectionObject($item);
-		$mets = var_export($r->getMethods());
+		//$r = new ReflectionObject($item);
+		//$mets = var_export($r->getMethods());
 		$this->view->item = $item;
 		//$this->view->gml = $gml;
 	}
