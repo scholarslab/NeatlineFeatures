c470635089901cd0c92ecf16c77ffd2a7410a4db
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-13 20:28:02 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@2036 d187d172-3112-0410-8ee7-ad866068e984

--- views/shared/javascripts/features/show/init.js
+++ views/shared/javascripts/features/show/init.js
@@ -1,16 +1,29 @@
 var init = function() {
 
 	wgs84 = new OpenLayers.Projection("EPSG:4326");
+	
+	var myStyles = new OpenLayers.StyleMap({
+        "default": new OpenLayers.Style({
+            fillColor: "#66ccff",
+            strokeColor: "#6600ff",
+            strokeWidth: 3
+        }),
+        "select": new OpenLayers.Style({
+            fillColor: "#66ccff",
+            strokeColor: "#3399ff"
+        })
+    });
+
 
 	map = new OpenLayers.Map('map', {
 		projection : wgs84,
 		numZoomLevels : 128
 	});
 
-	var featurelayer = new OpenLayers.Layer.Vector("feature");
+	var featurelayer = new OpenLayers.Layer.Vector("feature", { styleMap: myStyles });
 	featurelayer.addFeatures(feature);
 	map.addLayer(featurelayer);
-	console.log(layers);
+	//console.log(layers);
 	if (layers.length > 0) {
 		for (var i = 0; i < layers.length; i++) {
 				var backgroundlayer = new OpenLayers.Layer.WMS(layers[i].title,
@@ -18,7 +31,7 @@ var init = function() {
 							srs : "EPSG:4326",
 							layers : layers[i].layername,
 						})
-				console.log(backgroundlayer);
+				//console.log(backgroundlayer);
 				map.addLayer(backgroundlayer);
 			
 		}
