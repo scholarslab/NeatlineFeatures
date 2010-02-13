3157227a2211e0c3ce2f6ff211129d4823b056a8
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-13 19:15:09 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@2006 d187d172-3112-0410-8ee7-ad866068e984

--- controllers/FeaturesController.php
+++ controllers/FeaturesController.php
@@ -11,29 +11,69 @@ class NeatlineFeatures_FeaturesController extends Omeka_Controller_Action
 	{
 	}
 
-	
 	public function showAction()
 	{
 		$logger = Omeka_Context::getInstance()->getLogger();
 
 		$id = (!$id) ? $this->getRequest()->getParam('id') : $id;
 		$backgroundMap = (!$backgroundMap) ? $this->getRequest()->getParam('backgroundMap') : $backgroundMap;
-		
+
 		$item = $this->findById($id,"Item");
-		
-		//$r = new ReflectionObject($item);
-		//$mets = var_export($r->getMethods());
+
 		$this->view->item = $item;
-		$this->view->backgroundMap = $backgroundMap;
+		$backgroundMaps = explode($backgroundMap,',');
+
+		$backgroundLayers = array();
+		foreach ( $backgroundMaps as $mapid )
+		{
+			$map = $this->findById($mapid,"Item");
+			$layertitle = $map->getElementTextsByElementNameAndSetName( 'Title', 'Dublin Core');
+			$serviceaddy = $map->getServiceAddy();
+			$layername = $map->getLayerName();
+			$backgroundLayers[$layertitle] = array("layername" => $layername, "serviceaddy" => $serviceaddy);
+		}
 	}
-	
+
 	public function editAction()
 	{
-		
 	}
-	
 
-	
+	private function getServiceAddy($item)
+	{
+		try {
+			$serviceaddys = $item->getElementTextsByElementNameAndSetName( 'Service address', 'Item Type Metadata');
+		}
+		catch (Omeka_Record_Exception $e) {
+		}
 
+		if ($serviceaddys) {
+			$serviceaddy = $serviceaddys[0]->text;
+		}
+		if ($serviceaddy) {
+			return $serviceaddy;
+		}
+		else {
+			return NEATLINE_GEOSERVER . "/wms";
+		}
+	}
+
+	private function getLayerName($item)
+	{
+		try {
+			$serviceaddys = $item->getElementTextsByElementNameAndSetName( 'Layername', 'Item Type Metadata');
+		}
+		catch (Omeka_Record_Exception $e) {
+		}
+
+		if ($serviceaddys) {
+			$serviceaddy = $serviceaddys[0]->text;
+		}
+		if ($serviceaddy) {
+			return $serviceaddy;
+		}
+		else {
+			return NEATLINE_GEOSERVER_NAMESPACE_PREFIX . ":" . $item->id;
+		}
+	}
 
 }
