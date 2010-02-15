febb6dcb9a96fb3192a4d03c290316baa9637f77
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-15 15:49:33 +0000

    starting feature editor
    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@2039 d187d172-3112-0410-8ee7-ad866068e984

---
+++ views/admin/features/edit.php
@@ -0,0 +1,33 @@
+<?php
+	$wkt = item("Dublin Core","Coverage",$item);
+	head();
+?>
+
+		<title>Neatline feature display</title>
+		<link rel="stylesheet" href="http://dev.openlayers.org/releases/OpenLayers-2.8/theme/default/style.css" type="text/css" />
+
+		<script type="text/javascript" src="http://openlayers.org/api/OpenLayers.js">Ê</script>
+		<script type="text/javascript" defer="">
+		//<![CDATA[
+			feature = new OpenLayers.Format.WKT().read("<?php echo $wkt ?>");		
+			layers = new Array();
+			<?php 
+				foreach ($backgroundLayers as $layername => $layervalues) {
+ 				   ?> 
+ 				   layers.push( { "title":"<?php echo $layername ?>", 
+ 		 				   			"address":"<?php echo $layervalues["serviceaddy"] ?>",
+ 		 		 				   	"layername":"<?php echo $layervalues["layername"] ?>" } ) ;
+ 				   <?php 
+				}
+			?>
+			//]]> 	
+		</script>
+		<?php echo js("features/show/init"); ?>
+	</head>
+	<body onload="init()">
+		<?php echo $backgroundMap?>
+		 <div id="map" style="height: 400px; width: 700px; 
+border: 1px solid #ccc; float:right;"></div>
+		
+		 
+	</body>
