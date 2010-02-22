dffc8dbc0bea93396314217ef5f05e0923a0b1a8
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-22 17:00:46 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@2140 d187d172-3112-0410-8ee7-ad866068e984

--- plugin.php
+++ plugin.php
@@ -48,7 +48,7 @@ function neatlinefeatures_define_acl($acl)
 
 function neatlinefeatures_map_widget($html,$inputNameStem,$value,$options,$record,$element)
 {
-	return eval(partial('foo.phtml', array()));
+	return eval("partial('foo.phtml', array())");
 	//return eval(implode('',file(NEATLINEFEATURES_LIB_DIR . "edit.php")) );
 }
 
