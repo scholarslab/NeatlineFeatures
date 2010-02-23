dcd2f14850d37ac1712d45cf79f8bb8918ec065d
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-02-23 15:29:34 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@2184 d187d172-3112-0410-8ee7-ad866068e984

--- plugin.php
+++ plugin.php
@@ -49,7 +49,7 @@ function neatlinefeatures_map_widget($html,$inputNameStem,$value,$options,$recor
 {
 	$writer = new Zend_Log_Writer_Stream(LOGS_DIR . DIRECTORY_SEPARATOR . "neatline.log");
 	$logger = new Zend_Log($writer);
-	$div = __v()->partial('features/edit.phtml', array("logger" => $logger));
+	$div = __v()->partial('features/edit.phtml', array("logger" => $logger, "NEATLINEMAPS_ITEMTYPE" => NEATLINEMAPS_ITEMTYPE));
 	return $div;
 }
 
