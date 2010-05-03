9a40879b1e5cf5215722735843c2db8e666ed254
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-05-03 15:52:15 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@2497 d187d172-3112-0410-8ee7-ad866068e984

--- views/public/features/wkt.php
+++ views/public/features/wkt.php
@@ -1,1 +1,3 @@
-GEOMETRYCOLLECTION( <?php echo implode(',', $wkts) ?> )
+<?php if( count($wkts) > 1) { ?>
+GEOMETRYCOLLECTION( <?php echo implode(',', $wkts)  ?> )
+<?php } else { echo $wkts[0]; } ?>
