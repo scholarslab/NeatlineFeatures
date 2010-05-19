f040ca974148b5852a545d6edca89dee4a9bd17f
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-05-19 00:46:20 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@2594 d187d172-3112-0410-8ee7-ad866068e984

--- views/admin/javascripts/widgets/edit/edit.js
+++ views/admin/javascripts/widgets/edit/edit.js
@@ -113,7 +113,7 @@ controls = {
 		"height" : 'auto',
 		"title" : "Add a Layer...",
 		"closeOnEscape": true,
-		"open" : function(event, ui) { jQuery.get("addlayer"); });
+		"open" : function(event, ui) {  });
 
     controls.selectCtrl.activate();
     if (features) {  	
