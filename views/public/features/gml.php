4840e322156559dffd6fa19368dbbc34b078a062
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-10-12 16:19:19 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@3355 d187d172-3112-0410-8ee7-ad866068e984

--- views/public/features/gml.php
+++ views/public/features/gml.php
@@ -1,15 +1,11 @@
-<?php header('Content-Type: text/xml'); ?>
+<?php header('Content-Type: text/xml');
 $output = "<gml:FeatureCollection xmlns:gml=\"http://www.opengis.net/gml/3.2\"
 	gml:id=\"id\"
 	xmlns:n=\"http://" . $_SERVER['SERVER_NAME'] . '>';
-<?php
 foreach ($gmls as $gml) {
 	$output .= '<gml:featureMember> <n:feature gml:id=' . md5($gml) . '.feature"' . $gml .' </n:feature> </gml:featureMember>';
 }
-
-?>
 $output .= '</gml:FeatureCollection>';
-<?php 
 print $output;
 /*
  * 
