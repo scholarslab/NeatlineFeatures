bc19909af2951aeac9b91d32af5d8b09e44c1dbc
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-12-19 00:44:20 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@3682 d187d172-3112-0410-8ee7-ad866068e984

--- views/shared/javascripts/drawing-tools.js
+++ views/shared/javascripts/drawing-tools.js
@@ -5,7 +5,7 @@ if (!Omeka.NeatlineFeatures) {
 	Omeka.NeatlineFeatures = new Array();
 }
 Omeka.NeatlineFeatures.createDrawingControlPanel = function(featurelayer,inputNameStem,div) {
-	var annotatedialog = jQuery("<input name='name' id='featurename' value='Put a name here'/><textarea id='featuredescription' name='description'>Put a description here</textarea><label for='featurename'/><label for='featuredescription'/>");
+	var annotatedialog = jQuery("<div><input name='name' id='featurename' value='Put a name here'/><textarea id='featuredescription' name='description'>Put a description here</textarea><label for='featurename'/><label for='featuredescription'/></div>");
 	annotatedialog.appendTo(div);
 	var controls = [
 	        new OpenLayers.Control.ModifyFeature(featurelayer, {
