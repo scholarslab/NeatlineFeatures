9873544a4e84e7f6ae402199e8ca3e10aeb7e908
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-12-28 13:17:44 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineFeatures/trunk@3744 d187d172-3112-0410-8ee7-ad866068e984

--- controllers/FeaturesController.php
+++ controllers/FeaturesController.php
@@ -29,22 +29,56 @@ class NeatlineFeatures_FeaturesController extends Omeka_Controller_Action
 	
 	public function gmlAction()
 	{
-		$coverages = array();
-		$id = (!$id) ? $this->getRequest()->getParam('id') : $id;
-		$item = $this->findById($id,"Item");
-		try {
-			$coverages = $item->getElementTextsByElementNameAndSetName( 'Coverage', 'Dublin Core');
+		if ($this->getRequest()->isGet()) {	
+			$coverages = array();
+			$id = (!$id) ? $this->getRequest()->getParam('id') : $id;
+			$item = $this->findById($id,"Item");
+			try {
+				$coverages = $item->getElementTextsByElementNameAndSetName( 'Coverage', 'Dublin Core');
+			}
+			catch (Omeka_Record_Exception $e) {
+			}
+			$this->view->gmls = array();
+			foreach($coverages as $coverage) {
+				if ( $this->isGML($coverage->text) ) {
+					array_push($this->view->gmls, $coverage->text);
+				}
+			}
 		}
-		catch (Omeka_Record_Exception $e) {
-		}
-		$this->view->gmls = array();
-		foreach($coverages as $coverage) {
-			if ( $this->isGML($coverage->text) ) {
-				array_push($this->view->gmls, $coverage->text);
+		else {
+			// this must be a POST update
+			// we accept the body of the POST as GML to be persisted into the dc:coverage
+			$coverages = array();
+			$id = (!$id) ? $this->getRequest()->getParam('id') : $id;
+			$item = $this->findById($id,"Item");
+			try {
+				$coverages = $item->getElementTextsByElementNameAndSetName( 'Coverage', 'Dublin Core');
+			}
+			catch (Omeka_Record_Exception $e) {
+			}
+			$gmls_coverages = array();
+			foreach($coverages as $coverage) {
+				if ( $this->isGML($coverage->text) ) {
+					array_push($gmls_coverages, $coverage);
+				}
+			}
+			if (count($gml_coverages) > 0) {
+				// there is a coverage into which to persist
+				// use only the first for now
+				$coverage = $gml_coverages[0];
+				$coverage->setText($this->getRequest()->getBody);
+				$item->save();
+			}
+			else {
+				// this is an Item without pre-existing geodata
 			}
 		}
 	}
-
+	
+	private function isGML($i) {
+		return (stripos($i,"<gml") == 0) || (stripos($i,"<wfs") == 0) ;
+	}
+/*
 	private function isWKT($i)
 	{
 		$j = strtoupper( $this->strstrb($i, '(') );
@@ -76,13 +110,11 @@ class NeatlineFeatures_FeaturesController extends Omeka_Controller_Action
 		}
 		return false;
 	}
-	
-	private function isGML($i) {
-		return (stripos($i,"<gml") == 0) || (stripos($i,"<wfs") == 0) ;
-	}
-
+		
 	private function strstrb($h,$n){
 		return array_shift(explode($n,$h,2));
 	}
+*/
+
 }
 ?>
