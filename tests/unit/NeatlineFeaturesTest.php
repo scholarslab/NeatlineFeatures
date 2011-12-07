9366424b208059cd9fb61499cfc47dadaeff30c0
Eric Rochester <err8n@virginia.edu>
2011-12-07 16:46:23 -0500

    Added unit-testing infrastructure.

---
+++ tests/unit/NeatlineFeaturesTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This is a test just to test the test system. Testing my patience is more 
+ * like it.
+ **/
+class ClassName extends Omeka_Test_AppTestCase
+{
+    /**
+     * undocumented function
+     *
+     * @return void
+     * @author Eric Rochester
+     **/
+    public function testTesting()
+    {
+        $this->assertFalse(TRUE);
+    }
+}
