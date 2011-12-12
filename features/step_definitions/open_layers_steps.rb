a2310c976ab9786b824203095a29b17e0dae5c3e
Eric Rochester <err8n@virginia.edu>
2011-12-12 13:13:13 -0500

    Filled in steps.

--- features/step_definitions/open_layers_steps.rb
+++ features/step_definitions/open_layers_steps.rb
@@ -14,19 +14,21 @@ Then /^I should see a map in "([^"]*)"$/ do |parent|
 end
 
 Then /^I should see a map$/ do |arg1|
-  pending
+  find('.olMap').should be_visible
 end
 
 Then /^I should not see a map$/ do |arg1|
-  pending
+  find('.olMap').should_not be_visible
 end
 
 Then /^I should see an OpenLayers map$/ do
-  pending # express the regexp above with the code you wish you had
+  find('.olMap').should be_visible
 end
 
-Then /^I should see an OpenLayers map in the "([^"]*)" field$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^I should see an OpenLayers map in the "([^"]*)" field$/ do |parent|
+  within(parent) do
+    find(".olMap").should be_visible
+  end
 end
 
 Then /^a point is visible on a map$/ do
