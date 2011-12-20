caefbdd7654e4a1f5a333cfca1e207c90f20268d
Eric Rochester <err8n@virginia.edu>
2011-12-20 08:55:42 -0500

    Queue a file to removed after it's actually been copied.

--- features/step_definitions/omeka_steps.rb
+++ features/step_definitions/omeka_steps.rb
@@ -1,9 +1,9 @@
 
 Given /^I replace "([^"]*)" with "([^"]*)"$/ do |dest, src|
-  NeatlineFeatures.file_fixtures << dest
   dirname = File.dirname(dest)
   FileUtils.mkdir_p(dirname) if !Dir.exists?(dirname)
   FileUtils.cp(src, dest)
+  NeatlineFeatures.file_fixtures << dest
 end
 
 Then /^I should see the following output in unordered list "([^"]*)":$/ do |list_id, table|
