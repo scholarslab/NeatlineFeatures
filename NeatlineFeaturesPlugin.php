1efca0e3eee1a63c06cf1afecb4dcbb1542eed75
Eric Rochester <err8n@virginia.edu>
2012-02-13 12:21:51 -0500

    Added fields for the viewport to the features table.

--- NeatlineFeaturesPlugin.php
+++ NeatlineFeaturesPlugin.php
@@ -111,11 +111,13 @@ class NeatlineFeaturesPlugin
     {
         $sql = "
             CREATE TABLE IF NOT EXISTS `{$this->_db->prefix}neatline_features` (
-                id              INT(10)    UNSIGNED NOT NULL AUTO_INCREMENT,
-                added           TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
-                item_id         INT(10)    UNSIGNED NOT NULL,
-                element_text_id INT(10)    UNSIGNED NOT NULL,
-                is_map          TINYINT(1) NOT NULL DEFAULT 0,
+                id              INT(10)        UNSIGNED NOT NULL AUTO_INCREMENT,
+                added           TIMESTAMP      DEFAULT CURRENT_TIMESTAMP,
+                item_id         INT(10)        UNSIGNED NOT NULL,
+                element_text_id INT(10)        UNSIGNED NOT NULL,
+                is_map          TINYINT(1)     NOT NULL DEFAULT 0,
+                zoom            SMALLINT       NOT NULL DEFAULT 6,
+                center          DECIMAL(12, 9) NOT NULL DEFAULT 0.0,
                 CONSTRAINT PRIMARY KEY (id),
                 INDEX (item_id, element_text_id)
             ) ENGINE=innodb DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
