cfcd9c89f8dd93a8e968155f31f4e7bcb9db623e
Eric Rochester <err8n@virginia.edu>
2012-02-14 15:41:42 -0500

    Change the database table to track current data.

--- NeatlineFeaturesPlugin.php
+++ NeatlineFeaturesPlugin.php
@@ -116,6 +116,10 @@ class NeatlineFeaturesPlugin
                 item_id         INT(10)        UNSIGNED NOT NULL,
                 element_text_id INT(10)        UNSIGNED NOT NULL,
                 is_map          TINYINT(1)     NOT NULL DEFAULT 0,
+                wkt             TEXT           NOT NULL DEFAULT '',
+                zoom            SMALLINT(2)    NOT NULL DEFAULT 3,
+                center_lon      DECIMAL(12, 5) NOT NULL DEFAULT 0.0,
+                center_lat      DECIMAL(12, 5) NOT NULL DEFAULT 0.0,
                 CONSTRAINT PRIMARY KEY (id),
                 INDEX (item_id, element_text_id)
             ) ENGINE=innodb DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
