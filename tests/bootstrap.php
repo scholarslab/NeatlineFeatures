a5e3761a3e835f29b1acd605851b9045558016c9
Eric Rochester <err8n@virginia.edu>
2012-06-20 17:00:08 -0400

    Move test initialization code into a bootstrap file.

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!($omekaDir = getenv('OMEKA_DIR'))) {
+    $omekaDir = dirname(dirname(dirname(dirname(__FILE__))));
+}
+if (!defined('NEATLINE_FEATURES_PLUGIN_DIR')) {
+    define(
+        'NEATLINE_FEATURES_PLUGIN_DIR',
+        '..'
+    );
+}
+
+require_once $omekaDir . '/application/tests/bootstrap.php';
+require_once $omekaDir . '/plugins/NeatlineFeatures/NeatlineFeaturesPlugin.php';
+require_once 'NeatlineFeatures_Test.php';
+
