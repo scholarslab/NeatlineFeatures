4b3614dddcdcf79608ce03893d18568d5dfe01c5
Eric Rochester <erochest@virginia.edu>
2013-04-02 10:24:05 -0400

    Fixed bug in setting the component in the edit filter.

--- NeatlineFeaturesPlugin.php
+++ NeatlineFeaturesPlugin.php
@@ -299,12 +299,9 @@ class NeatlineFeaturesPlugin extends Omeka_Plugin_AbstractPlugin
             $args['input_name_stem'],
             $args['index']
         );
-        // echo "<script>console.log('";
-        // echo str_replace("\n", "\\n", str_replace("'", "\\'", print_r($components, true)));
-        // echo "');</script>";
         // Default $components['inputs']:
         // <div class="input-block"><div class="input"><textarea name="Elements[38][0][text]" id="Elements-38-0-text" rows="3" cols="50"></textarea></div><div class="controls"><input type="submit" name="" value="Remove" class="remove-element red button"></div><label class="use-html">Use HTML<input type="hidden" name="Elements[38][0][html]" value="0"><input type="checkbox" name="Elements[38][0][html]" id="Elements-38-0-html" value="1" class="use-html-checkbox"></label></div>
-        $components['inputs'] = $util->getEditControl();
+        $components['input'] = $util->getEditControl();
 
         return $components;
     }
